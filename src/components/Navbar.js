ce6cd83a632573a4efcce5b8216ca5d4f0d089d1
marianjuli <marian.pal@gmail.com>
2022-04-18 21:52:41 -0300

    Primer commit

---
+++ src/components/Navbar.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+
+const Navbar = ({brand}) => {
+  return (
+    <nav className="navbar navbar-dark bg-dark">
+    <div className="container">
+    <a   className="navbar-brand text-uppercase" href="/">Rick and Morty App</a>
+    
+    </div>
+    
+    
+    </nav>
+    
+  )
+}
+
+export default Navbar
